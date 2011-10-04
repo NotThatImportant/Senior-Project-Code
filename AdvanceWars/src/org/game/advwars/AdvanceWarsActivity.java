7b8a2ec1d6d1dafa130ce020843054b5f3322f34
statuskwo5 <statuskwo5@yahoo.com>
2011-10-04 14:23:40 -0400

    Added the Android project to repository.

---
+++ AdvanceWars/src/org/game/advwars/AdvanceWarsActivity.java
@@ -0,0 +1,51 @@
+package org.game.advwars;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+
+/**
+ * Main Android class
+ * @author Sinisa Malbasa
+ *
+ */
+public class AdvanceWarsActivity extends Activity implements OnClickListener
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        // Set up click listeners for all buttons
+        View previousPlayerName = findViewById(R.id.previous_player_name);
+        previousPlayerName.setOnClickListener(this);
+        View newPlayerName = findViewById(R.id.new_player_name);
+        newPlayerName.setOnClickListener(this);
+        View skip = findViewById(R.id.skip);
+        skip.setOnClickListener(this);
+    }
+    
+    public void onClick(View v)
+    {
+    	switch (v.getId())
+    	{
+    	case R.id.previous_player_name:
+    		Intent i = new Intent(this, About.class);
+    		startActivity(i);
+    		break;
+    	case R.id.new_player_name:
+    		Intent i2 = new Intent(this, About.class);
+    		startActivity(i2);
+    		break;
+    	case R.id.skip:
+    		Intent i3 = new Intent(this, About.class);
+    		startActivity(i3);
+    		break;
+    	}
+    }
+}
---
+++ AdvanceWars/src/org/game/advwars/MainMenu.java
@@ -0,0 +1,13 @@
+package org.game.advwars;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainMenu extends Activity
+{
+	protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+    }
+}
