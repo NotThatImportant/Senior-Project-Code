80d10a14fa7612aca20715b3499807940bd6f33d
statuskwo5 <statuskwo5@SpaceOrbitalLT.(none)>
2011-11-22 16:38:04 -0500

    Made more changes to the Scores view and got it to display player wins, losses, and experience.

--- AdvanceWars/src/org/game/advwars/LeaderBoard.java
+++ AdvanceWars/src/org/game/advwars/LeaderBoard.java
@@ -3,7 +3,11 @@ package org.game.advwars;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import dataconnectors.DBAndroidConnector;
+
 import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -11,13 +15,22 @@ import android.widget.SimpleAdapter;
 
 public class LeaderBoard extends Activity
 {
+	private int index = 0;
+	private ArrayList<String> playerNames = new ArrayList<String>();
+	private ArrayList<String> playerWins = new ArrayList<String>();
+	private ArrayList<String> playerLosses = new ArrayList<String>();
+	private ArrayList<String> playerXP = new ArrayList<String>();
+	
 	protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.leader_board);
         
-        ListView list = (ListView) findViewById(R.id.scores);
+        // Get player name, wins, losses, and XP from database
+        getPlayerInfo();
         
+        ListView list = (ListView) findViewById(R.id.scores);
+
         ArrayList<HashMap<String, String>> mylist = new ArrayList<HashMap<String, String>>();
         HashMap<String, String> map = new HashMap<String, String>();
         map.put("PlayerName", "Player Name");
@@ -25,51 +38,81 @@ public class LeaderBoard extends Activity
         map.put("Losses", "Losses");
         map.put("XP", "Experience");
         mylist.add(map);
-        map = new HashMap<String, String>();
-        map.put("PlayerName", "103(x)");
-        map.put("Wins", "6:35 AM");
-        map.put("Losses", "7:45 AM");
-        map.put("XP", "7:40 AM");
-        mylist.add(map);
-        map = new HashMap<String, String>();
-        map.put("PlayerName", "103(x)");
-        map.put("Wins", "6:35 AM");
-        map.put("Losses", "7:45 AM");
-        map.put("XP", "7:40 AM");
-        mylist.add(map);
-        map = new HashMap<String, String>();
-        map.put("PlayerName", "103(x)");
-        map.put("Wins", "6:35 AM");
-        map.put("Losses", "7:45 AM");
-        map.put("XP", "7:40 AM");
-        mylist.add(map);
-        map = new HashMap<String, String>();
-        map.put("PlayerName", "103(x)");
-        map.put("Wins", "6:35 AM");
-        map.put("Losses", "7:45 AM");
-        map.put("XP", "7:40 AM");
-        mylist.add(map);
-        map = new HashMap<String, String>();
-        map.put("PlayerName", "103(x)");
-        map.put("Wins", "6:35 AM");
-        map.put("Losses", "7:45 AM");
-        map.put("XP", "7:40 AM");
-        mylist.add(map);
-        map = new HashMap<String, String>();
-        map.put("PlayerName", "103(x)");
-        map.put("Wins", "6:35 AM");
-        map.put("Losses", "7:45 AM");
-        map.put("XP", "7:40 AM");
-        mylist.add(map);
-        map = new HashMap<String, String>();
-        map.put("PlayerName", "103(x)");
-        map.put("Wins", "6:35 AM");
-        map.put("Losses", "7:45 AM");
-        map.put("XP", "7:40 AM");
-        mylist.add(map);
+        
+        while (index < playerNames.size())
+        {
+        	map = new HashMap<String, String>();
+            map.put("PlayerName", playerNames.get(index));
+            map.put("Wins", playerWins.get(index));
+            map.put("Losses", playerLosses.get(index));
+            map.put("XP", playerXP.get(index));
+            mylist.add(map);
+            
+            index++;
+        }
 
         ListAdapter mSchedule = new SimpleAdapter(this, mylist, R.layout.leader_board,
                     new String[] {"PlayerName", "Wins", "Losses", "XP"}, new int[] {R.id.player_name, R.id.wins, R.id.losses, R.id.xp});
         list.setAdapter(mSchedule);
     }
+	
+	private void getPlayerInfo()
+	{
+		DBAndroidConnector db = new DBAndroidConnector();
+        SQLiteDatabase myDataBase = db.getDB();
+		
+		try
+        {
+        	Cursor cursor = myDataBase.rawQuery("select Player_Name from Scores", null);
+
+            if(cursor.moveToFirst())
+            {
+                do
+                {
+                    playerNames.add(cursor.getString(0));
+                }
+                while(cursor.moveToNext());
+            }
+            
+        	cursor = myDataBase.rawQuery("select Wins from Scores", null);
+
+            if(cursor.moveToFirst())
+            {
+                do
+                {
+                    playerWins.add(cursor.getString(0));
+                }
+                while(cursor.moveToNext());
+            }
+            
+        	cursor = myDataBase.rawQuery("select Losses from Scores", null);
+
+            if(cursor.moveToFirst())
+            {
+                do
+                {
+                    playerLosses.add(cursor.getString(0));
+                }
+                while(cursor.moveToNext());
+            }
+            
+        	cursor = myDataBase.rawQuery("select Experience from Scores", null);
+
+            if(cursor.moveToFirst())
+            {
+                do
+                {
+                    playerXP.add(cursor.getString(0));
+                }
+                while(cursor.moveToNext());
+            }
+            
+            cursor.close();
+
+        }
+        finally
+        {
+            myDataBase.close();
+        }
+	}
 }
